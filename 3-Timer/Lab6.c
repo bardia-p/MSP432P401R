5d3a36884f44b51a8a2138470d7573d19a4f4710
Bardia Parmoun <bardiaparmoun@gmail.com>
2021-11-27 14:11:27 -0500

    added the code for lab 6.

--- 3-Timer/Lab6.c
+++ 3-Timer/Lab6.c
@@ -24,41 +24,46 @@ void config_leds(void);
 void config_interrupts(void);
 
 // Variable definitions
-static volatile uint8_t current_led;
+static volatile uint8_t currentLED;
+static volatile uint8_t paused;
 
-int main(void) {
-    // Disable the watchdog timer
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+int main() {
+  // RED LED is selected by default
+	currentLED = LED_RED;
 
-	// Configure GPIO switchs and LEDs.
+  // The system originally is not paused
+	paused = 0;
+	
+  // Disable the watchdog timer
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+
+	// Configure GPIO switchs and LEDs
 	config_switches();
 	config_leds();
+
+  // Configure interrupts
+  config_interrupts();
 	
-	// Check initial LED selection is set to RED
-	current_led = LED_RED;
-
-    // Globally enable interrupts in CPU
-    __ASM("CPSIE I");
-
-    // Configure interrupts
-    config_interrupts();
-
+	TA0CTL &= (uint16_t)(~((1<<5)|(1<<4)));     // Stop the timer
+	TA0CTL &= (uint16_t)(~(1<<0));              // Clear interrupt flag
+	TA0CCR0 = (uint16_t)(32768);                // Holds the upper limit value
+	TA0CTL |= (uint16_t)((1<<1));               // Interrupt enable
+	TA0CTL |= (uint16_t)((1<<4));               // Up count mode
+	TA0CTL |= (uint16_t)((1<<8));               // ACLK as source for timer -> 32.768 kHz
+								
 	// NVIC configuration
 	NVIC_SetPriority(PORT1_IRQn,2);
 	NVIC_ClearPendingIRQ(PORT1_IRQn);
 	NVIC_EnableIRQ(PORT1_IRQn);
-
-    // Setup Timer
-    TA0->CCTL0 = TIMER_A_CCTLN_CCIE;        // Enable CC interrupt
-    TA0->CTL = TIMER_A_CTL_TASSEL_1 |       // ACLK as source for timer -> 32.768 kHz
-                TIMER_A_CTL_ID_0 |          // Divide clock by 1 -> 32.768 kHz / 1 = 32.768 kHz
-                TIMER_A_CTL_MC_1 |          // Up mode
-                TIMER_A_CTL_IE;             // Enable overflow interrupt
-
-    // Holds the upper limit value
-    TA0->CCR0 = 32768;                      // Timer 16 bits range gives 2 seconds, we want every 1 second
-
-	// Create infinite loop.
+	
+	NVIC_SetPriority(TA0_N_IRQn,3);
+	NVIC_ClearPendingIRQ(TA0_N_IRQn);
+	NVIC_EnableIRQ(TA0_N_IRQn);
+	
+	//Globally enable interrupts in CPU
+	__ASM("CPSIE I");
+	
+	// Waiting for interrupts
 	while(1) {
 		__ASM("WFI");
 	}
@@ -69,7 +74,7 @@ int main(void) {
 /* 
 Configure the switches (P1.1 and P1.4) as inputs, using pull-up internal resistors. 
 */
-void config_switches(void) {
+void config_switches(){	
 	// Set function to GPIO (P1.1 and P1.4)
 	P1->SEL0 &= (uint8_t)(~((1<<1)|(1<<4)));
 	P1->SEL1 &= (uint8_t)(~((1<<1)|(1<<4)));
@@ -87,7 +92,7 @@ void config_switches(void) {
 /*
 Configure LEDs (P1.0 and P2.0, P2.1, P2.2) as outputs.
 */
-void config_leds(void) {
+void config_leds(){	
     /* LEDs P1.0 */
 	// Set function to GPIO
 	P1->SEL0 &= (uint8_t)(~(1<<0));
@@ -137,57 +142,56 @@ void config_interrupts(void) {
 /*
 Port 1 interrupt service request handler.
 */
-void PORT1_IRQHandler(void) {
-	//Test for pin 3 interrupt flag (Button 1 pressed)
-	if ((P1->IFG & (uint8_t)(1<<1)) != 0) {
-        // Debouncing the LEDs
-	    static uint16_t i = DELAY_VALUE;
-        while (i--) {
-            __ASM volatile ("") // Delay loop contains some asm code placeholder
-        }
-
-		// Clearing the interrupt flag
-		P1->IFG &= ~(uint8_t)(1<<1);
+void PORT1_IRQHandler(void){
+	static volatile uint16_t i = DELAY_VALUE;
+	
+	if (((P1IFG & (uint8_t)(1<<1)) != 0) || ((P1IFG & (uint8_t) (1<<4)) != 0)) {
+    // Debouncing the LEDs
+    while (i--) {
+			__ASM volatile (""); // Delay loop contains some asm code placeholder
+    }
 		
-		// Button 1 toggles between LEDs
-        // When the button is pressed, the currently selected LED retains its state
-		current_LED = (current_LED + 1) % 2;
+		// Test for pin 3 interrupt flag
+		if ((P1IFG & (uint8_t)(1<<1)) != 0) {
+            // Clearing the interrupt flag
+			P1IFG &= (uint8_t)~(1<<1);
 
-	} else if ((P1->IFG & (uint8_t)(1<<4)) != 0) {
-        // Debouncing the LEDs
-	    static uint16_t i = DELAY_VALUE;
-        while (i--) {
-            __ASM volatile ("") // Delay loop contains some asm code placeholder
-        }
-
-		// Clearing the interrupt flag
-		P1->IFG &= ~(uint8_t)(1<<4);
-		
-        // Button 2 pauses/resumes the system
-        // When paused, if LEDs were off, they should remain off
-        // Timer A Control Register Mode Control: Up Mode (p.797)
-        TA0->CTL ^= TIMER_A_CTL_MC_1;
+            // Button 1 toggles between LEDs
+            // When the button is pressed, the currently selected LED retains its state
+			currentLED = (currentLED + 1) % 2;
+		} else if((P1IFG & (uint8_t) (1<<4)) != 0) {
+			P1IFG &= (uint8_t)~(1<<4);
+			
+            // When paused, if LEDs were off, they should remain off
+			if (!paused) {
+				paused = 1;
+				TA0CTL &= (uint16_t)(~(1<<1)); // Disable interrupt
+				TA0CTL &= (uint16_t)(~((1<<5)|(1<<4))); // Stop timer
+			} else {
+				paused = 0;
+				TA0CTL |= (uint16_t)((1<<1)); // Interrupt enable
+				TA0CTL |= (uint16_t)((1<<4)); // Up count mode
+			}
+		}
 	}
 }
 
 /*
 Timer A interrupt request handler.
 */
-void TA0_N_IRQHandler(void) {
-    // TIMER_A_CTL_IFG = (uint16_t)(1<<0);
-
-    if (TA0->CTL & TIMER_A_CTL_IFG) {
-		if (currentLED == LED_RED) {
-            // RED_LED has 2 states, bitwise toggling
-			P1->OUT ^= (uint8_t)((1<<0));
-		} else {
-            // RGB_LED has 8 states, bitwise clearing and setting
-			uint8_t count = (P2->OUT & RGB_LED_MASK);
-			count++;
-			P2->OUT &= ~RGB_LED_MASK;
-			P2->OUT |= count & RGB_LED_MASK;
-		}
-        // Clear Timer A Control Register interrupt flag (p.797)
-        TA0->CTL &= ~TIMER_A_CTL_IFG;
-    }
+void TA0_N_IRQHandler(void){
+	// Clear the interrupt flag
+	TA0CTL &= (uint16_t) (~(1<<0));
+	
+	// Change the LEDs
+	if (currentLED == LED_RED){ 
+        // RED_LED has 2 states, bitwise toggling
+		P1->OUT ^= (uint8_t)((1<<0));
+	} else {
+        // RGB_LED has 8 states, bitwise clearing and setting
+		uint8_t count = (P2->OUT & RGB_LED_MASK);
+		count++;
+		P2->OUT &= ~(RGB_LED_MASK);
+		P2->OUT |= count & RGB_LED_MASK;
+	}
 }
